fbe34d6fee5845223ba495428a70c190d3d9f4a5
ninomiyayu <chanlonzxy2184@yahoo.co.jp>
2023-09-25 18:44:13 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let timer;
+let isRunning = false;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+function startStop() {
+    if (isRunning) {
+        clearInterval(timer);
+        document.getElementById("startStop").textContent = "スタート";
+    } else {
+        timer = setInterval(updateTime, 1000);
+        document.getElementById("startStop").textContent = "ストップ";
+    }
+    isRunning = !isRunning;
+}
+
+function reset() {
+    clearInterval(timer);
+    isRunning = false;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    updateDisplay();
+    document.getElementById("startStop").textContent = "スタート";
+}
+
+function updateTime() {
+    seconds++;
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    updateDisplay();
+}
+
+function updateDisplay() {
+    const timeDisplay = document.getElementById("display");
+    timeDisplay.textContent =
+        (hours < 10 ? "0" : "") + hours + ":" +
+        (minutes < 10 ? "0" : "") + minutes + ":" +
+        (seconds < 10 ? "0" : "") + seconds;
+}
+
+reset();
